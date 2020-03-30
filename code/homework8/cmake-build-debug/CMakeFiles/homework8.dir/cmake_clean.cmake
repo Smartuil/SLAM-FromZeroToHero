989671519d7a1247d3bfd49e483d6d5faa7ab18d
smartuil <563412673@qq.com>
2020-03-30 21:57:01 +0800

    homework7 8

---
+++ code/homework8/cmake-build-debug/CMakeFiles/homework8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework8.dir/draw_trajectory.cpp.o"
+  "homework8"
+  "homework8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
