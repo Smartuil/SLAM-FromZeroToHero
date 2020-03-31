15b6c1c4d4cedc1e6fcb5ecfaea3b3ab81425545
smartuil <563412673@qq.com>
2020-03-31 15:03:41 +0800

    homework9 10

---
+++ code/homework9/cmake-build-debug/CMakeFiles/homework9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework9.dir/undistortImage.cpp.o"
+  "homework9"
+  "homework9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
