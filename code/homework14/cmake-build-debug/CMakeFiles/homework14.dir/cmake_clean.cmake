876e2273fe7abd14309a8f4b857b2ae0d6a21de0
smartuil <563412673@qq.com>
2020-04-12 17:33:09 +0800

    homework14

---
+++ code/homework14/cmake-build-debug/CMakeFiles/homework14.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework14.dir/main.cpp.o"
+  "homework14"
+  "homework14.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/homework14/cmake-build-debug/src/CMakeFiles/pointCloudFilter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pointCloudFilter.dir/pointCloudFilter.cpp.o"
+  "pointCloudFilter"
+  "pointCloudFilter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointCloudFilter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
