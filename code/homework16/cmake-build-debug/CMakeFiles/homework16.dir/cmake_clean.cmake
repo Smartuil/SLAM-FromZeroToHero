97649a51c7073af233734f5c47d6f0c685738404
smartuil <563412673@qq.com>
2020-04-16 15:41:57 +0800

    homework16

---
+++ code/homework16/cmake-build-debug/CMakeFiles/homework16.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework16.dir/main.cpp.o"
+  "homework16"
+  "homework16.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/homework16/cmake-build-debug/src/CMakeFiles/pointCloud2Mesh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pointCloud2Mesh.dir/pointCloud2Mesh.cpp.o"
+  "pointCloud2Mesh"
+  "pointCloud2Mesh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointCloud2Mesh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
