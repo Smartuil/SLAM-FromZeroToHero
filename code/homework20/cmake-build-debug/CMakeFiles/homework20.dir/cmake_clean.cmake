e37ab4bb17123dd63518f28542a99db28a332af8
smartuil <563412673@qq.com>
2020-05-18 16:52:20 +0800

    homework20

---
+++ code/homework20/cmake-build-debug/CMakeFiles/homework20.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework20.dir/main.cpp.o"
+  "homework20"
+  "homework20.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/homework20/cmake-build-debug/CMakeFiles/homework20.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for homework20.
+# This may be replaced when dependencies are built.
